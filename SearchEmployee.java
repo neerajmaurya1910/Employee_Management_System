bcb311e47ef6ad4dea4d3c7b83d594f9ee237490
neerajmaurya1910 <neerajmaurya1910@gmail.com>
2026-07-28 22:50:53 +0530

    Create SearchEmployee.java

---
+++ SearchEmployee.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SearchEmployee {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int storedID = 101;
+        String storedName = "Rahul";
+
+        System.out.print("Enter Employee ID: ");
+        int id = sc.nextInt();
+
+        if(id == storedID){
+            System.out.println("Employee Found");
+            System.out.println("Name: " + storedName);
+        } else{
+            System.out.println("Employee Not Found");
+        }
+
+        sc.close();
+    }
+}
