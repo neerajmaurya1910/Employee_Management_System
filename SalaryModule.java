8fdb0bc9ae6ef347ba191fbd0f742312c8b310ef
neerajmaurya1910 <neerajmaurya1910@gmail.com>
2026-07-28 22:47:05 +0530

    Create SalaryModule.java

---
+++ SalaryModule.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SalaryModule {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Basic Salary: ");
+        double basic = sc.nextDouble();
+
+        double hra = basic * 0.20;
+        double da = basic * 0.10;
+        double total = basic + hra + da;
+
+        System.out.println("HRA = " + hra);
+        System.out.println("DA = " + da);
+        System.out.println("Total Salary = " + total);
+
+        sc.close();
+    }
+}
