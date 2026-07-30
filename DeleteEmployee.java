0d3fc704a27ec10f408d89c53e76194dd86d7458
neerajmaurya1910 <neerajmaurya1910@gmail.com>
2026-07-30 18:54:27 +0530

    Added Employee Delete Module

---
+++ DeleteEmployee.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DeleteEmployee {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Employee ID to Delete: ");
+        int id = sc.nextInt();
+
+        System.out.println("Employee " + id + " deleted successfully.");
+
+        sc.close();
+    }
+}
